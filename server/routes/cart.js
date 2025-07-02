53a3a86a83772a3f27b8c734dc712221f91f9b51
Majid Husain <majidsmart7@gmail.com>
2025-07-02 11:04:06 +0530

    fix: Correct import case for CartSchema in cart route

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Cart = require("../models/cartSchema");
+const Cart = require("../models/CartSchema");
 const mongoose = require("mongoose");
 
 // 👉 POST: Add product to cart
